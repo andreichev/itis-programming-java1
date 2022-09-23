ec9c666241042d33749d91002320c01d751becce
Andreichev Michail <andreichev.m@mail.ru>
2022-09-23 09:20:01 +0300

    task 71 v1

---
+++ SimplePrograms5/src/task71_acmp_v1/Task71Acmp.java
@@ -0,0 +1,37 @@
+package task71_acmp_v1;
+
+import java.util.Scanner;
+
+public class Task71Acmp {
+    // 3
+    // 1 13 14
+    // 0
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] x = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = scanner.nextInt();
+        }
+
+        int minDelta = Integer.MAX_VALUE;
+        for (int i = 0; i < Math.pow(2, n); i++) {
+            int t = i;
+            int delta = 0;
+            for (int j = 0; j < n; j++) {
+                if(t % 2 == 1) {
+                    delta += x[j];
+                } else {
+                    delta -= x[j];
+                }
+                t /= 2;
+            }
+            if(Math.abs(delta) < minDelta) {
+                minDelta = Math.abs(delta);
+            }
+        }
+
+        System.out.println(minDelta);
+    }
+}
