a7d4483ddcfbc737f1b24beac6b071be84b8aa77
Andreichev Michail <andreichev.m@mail.ru>
2023-09-17 16:25:43 +0300

    inyan fixed

--- 2. Functions/Sandbox/src/inyan/Main.java
+++ 2. Functions/Sandbox/src/inyan/Main.java
@@ -7,21 +7,21 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int size = 30;
+        int size = 50;
         int bigCircleCenterX = size / 2;
         int bigCircleCenterY = size / 2;
         int bigCircleRadius = size / 2;
 
         float smallCircle1CenterX = size / 2.5f;
-        float smallCircle1CenterY = size / 3.5f;
-        float smallCircle1Radius = size / 6.f;
+        float smallCircle1CenterY = size / 4.0f;
+        float smallCircle1Radius = size / 7.f;
 
         float smallCircle2CenterX = size - size / 2.5f;
-        float smallCircle2CenterY = size - size / 3.5f;
-        float smallCircle2Radius = size / 6.f;
+        float smallCircle2CenterY = size - size / 4.0f;
+        float smallCircle2Radius = size / 7.f;
 
         for(float y = 0; y < size; y++) {
-            for(float x = 0; x < size; x += 0.35) {
+            for(float x = 0; x < size; x += 0.45) {
                 if(isPointInsideCircle(x, y, bigCircleCenterX, bigCircleCenterY, bigCircleRadius)) {
                     if(0.5 * Math.sin(y * 6 / size) <= (2 * x / size) - 1) {
                         if(isPointInsideCircle(x, y, smallCircle2CenterX, smallCircle2CenterY, smallCircle2Radius)) {
